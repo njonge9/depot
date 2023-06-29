803d89dba91b09895526969d417648c8d051a88d
njonge9 <frednjonge9@gmail.com>
2023-06-29 21:46:55 +0300

    Authenticating users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get 'admin'=>'admin#index'
+  controller :sesssions do
+    get 'login'=>:new
+    post 'login'=>:create
+    delete 'logout'=>:destroy
+  end
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   resources :users
   get 'stimulus/payment'
   resources :orders
