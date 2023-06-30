f22fad21b0963fa74f3159f09ff8ae8e68dd71c1
njonge9 <frednjonge9@gmail.com>
2023-06-30 09:02:43 +0300

    Remove the users

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true
-  validates :name, uniqueness: true
-  has_secure_password
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,14 @@
 Rails.application.routes.draw do
-  get 'admin'=>'admin#index'
-  controller :sesssions do
-    get 'login'=>:new
-    post 'login'=>:create
-    delete 'logout'=>:destroy
-  end
-  get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  resources :users
-  get 'stimulus/payment'
+  # get 'stimulus/payment'
   resources :orders
   resources :line_items
-  resources :carts, only: [:show ,:destroy]
+  resources :carts
   root 'store#index', as: 'store_index'
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
