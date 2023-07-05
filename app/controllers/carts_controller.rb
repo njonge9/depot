b394a88bd044e1ff64f0a468995355386fbced72
njonge9 <frednjonge9@gmail.com>
2023-07-05 14:03:36 +0300

    Finalizing user roles

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,5 @@
 class CartsController < ApplicationController
-  before_action :authorize_admin!
+  before_action :authorize_admin!, except: [:destroy]
   before_action :authenticate_user!
   before_action :set_cart, only: %i[ show edit update destroy ]
   rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
