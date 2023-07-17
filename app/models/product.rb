49b93f46fe013f5f10bee34f1b5c0241fe516064
njonge9 <frednjonge9@gmail.com>
2023-07-17 11:28:16 +0300

    Adding a rails admin dashboard

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,12 @@ class Product < ApplicationRecord
     validates :title, uniqueness: true
     has_rich_text :description
     has_one_attached :image
+
+    def image_as_thumbnail
+        image.variant(resize_to_limit: [200, 200]).processed
+    end
+
+
     private
     # ensure that there are no line items referencing this product
     def ensure_not_referenced_by_any_line_item
